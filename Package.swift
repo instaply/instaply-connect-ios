39fa9c17d3322c6c81b0c529974ff8f755f17c7b
Ferran <ferran.abello@gmail.com>
2021-03-29 13:45:32 +0200

    Try to publish the binary as part of the repo

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "InstaplyConnect",
-            url: "https://github.com/instaply/instaply-connect-ios-binaries/releases/download/1.0.0-alpha/InstaplyConnect.xcframework.zip",
-            checksum: "6d43db97967b8ef7fa6bd868747edac62f53f9838ec4ccc7db6beaf62321e77d"
+            path: "binary/InstaplyConnect.xcframework"
         )
     ]
 )
---
+++ binary/InstaplyConnect.xcframework/ios-arm64/InstaplyConnect.framework/Headers/InstaplyConnect.h
@@ -0,0 +1,19 @@
+//
+//  InstaplyConnect.h
+//  InstaplyConnect
+//
+//  Created by Ferran on 12/02/2020.
+//  Copyright © 2020 Instaply. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstaplyConnect.
+FOUNDATION_EXPORT double InstaplyConnectVersionNumber;
+
+//! Project version string for InstaplyConnect.
+FOUNDATION_EXPORT const unsigned char InstaplyConnectVersionString[];
+
+// In this header, you should import all the headers of your framework using statements like #import <InstaplyConnect/PublicHeader.h>
+
+
